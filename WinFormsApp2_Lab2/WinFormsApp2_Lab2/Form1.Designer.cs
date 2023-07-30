e0ff4e6fb5a6db80531f634238ed96babcc12d45
ErrantWanderrer <45843314+ErrantWanderrer@users.noreply.github.com>
2023-07-30 17:53:57 +0300

    Add files via upload

---
+++ WinFormsApp2_Lab2/WinFormsApp2_Lab2/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace WinFormsApp2_Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(327, 161);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(516, 160);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Большее основание";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Меньшее основание";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(516, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Высота";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Рассчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(254, 252);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(394, 252);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(236, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Периметр трапеции";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(394, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Площадь трапеции";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Ввод";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 351);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Вывод";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(680, 140);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 19);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Trap";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(680, 165);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 19);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Trap1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(680, 190);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 19);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Trap2";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ WinFormsApp2_Lab2/WinFormsApp2_Lab2/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2_Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Trapezoid trap = new();
+        Trapezoid trap1 = new();
+        Trapezoid trap2 = new(15, 10, 5);
+        Trapezoid trapLink;
+
+        public class Trapezoid
+        {
+            private float lengthHigh;
+            private float lengthLow;
+            private float height;
+
+            public Trapezoid()
+            {
+                lengthHigh = 20;
+                lengthLow = 15;
+                height = 4;
+            }
+            public Trapezoid(float high, float low, float h)
+            {
+                lengthHigh = high;
+                lengthLow = low;
+                height = h;
+            }
+
+            public float GetLengthHigh()
+            {
+                return lengthHigh;
+            }
+
+            public float GetLengthLow()
+            {
+                return lengthLow;
+            }
+
+            public float GetHeight()
+            {
+                return height;
+            }
+
+            public void SetLengthHigh(float high)
+            {
+                if (high < 0)
+                {
+                    MessageBox.Show("Значение должно быть больше нуля");
+                }
+                else
+                {
+                    lengthHigh = high;
+                }
+            }
+
+            public void SetLengthLow(float low)
+            {
+                if (low < 0)
+                {
+                    MessageBox.Show("Значение должно быть больше нуля");
+                }
+                else
+                {
+                    lengthLow = low;
+                }
+            }
+
+            public void SetHeight(float h)
+            {
+                if (h<1 || h>8)
+                {
+                    MessageBox.Show("Высота должна быть задана в границах от: 1 до 8");
+                }
+                else
+                {
+                    height = h;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Trapezoid trap = new();
+            trap.SetLengthHigh(Convert.ToSingle(textBox1.Text));
+            trap.SetLengthLow(Convert.ToSingle(textBox2.Text));
+            trap.SetHeight(Convert.ToSingle(textBox3.Text));
+            textBox4.Text = (trap.GetLengthHigh() + trap.GetLengthLow() + 2 * Math.Sqrt(Math.Pow(trap.GetHeight(), 2) + (trap.GetLengthHigh() - Math.Pow(trap.GetLengthLow(), 2)) / 4)).ToString();
+            textBox5.Text = ((trap.GetHeight() * (trap.GetLengthHigh() + trap.GetLengthLow()))/2).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //if ((radioButton1.Checked = true) || (radioButton2.Checked = true) || (radioButton3.Checked = true))
+            //{
+                trapLink.SetLengthHigh(15);
+                trapLink.SetLengthLow(10);
+                trapLink.SetHeight(5);
+            //}
+            //else
+              //  MessageBox.Show("объект не выбран");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //if ((radioButton1.Checked = true) || (radioButton2.Checked = true) || (radioButton3.Checked = true))
+            //{
+                textBox1.Text = trapLink.GetLengthHigh().ToString();
+                textBox2.Text = trapLink.GetLengthLow().ToString();
+                textBox3.Text = trapLink.GetHeight().ToString();
+                textBox4.Text = (trapLink.GetLengthHigh() + trapLink.GetLengthLow() + 2 * Math.Sqrt(Math.Pow(trapLink.GetHeight(), 2) + (trapLink.GetLengthHigh() - Math.Pow(trapLink.GetLengthLow(), 2)) / 4)).ToString();
+                textBox5.Text = ((trapLink.GetHeight() * (trapLink.GetLengthHigh() + trapLink.GetLengthLow())) / 2).ToString();
+            //}
+            //else
+               // MessageBox.Show("объект не выбран");       
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            trapLink = trap;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            trapLink = trap1;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            trapLink = trap2;
+        }
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //string text = textBox1.Text;
+            //trap.lengthHigh = Convert.ToSingle(text);
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //string text = textBox2.Text;
+            //trap.lengthLow = Convert.ToSingle(text);
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //string text = textBox3.Text;
+            //trap.height = Convert.ToSingle(text);
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
