7f72a41b5e4675e62cb915ac1f9035824ffab6d6
ErrantWanderrer <45843314+ErrantWanderrer@users.noreply.github.com>
2023-07-30 18:05:59 +0300

    Add files via upload

---
+++ WinFormsApp5/WinFormsApp5/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace WinFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(23, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 396);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Активный объект: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Прямоугольник";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Прямоугольный треугольник";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Правильный пятиугольник";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Точка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Location = new System.Drawing.Point(414, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(355, 396);
+            this.panel2.TabIndex = 1;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(185, 102);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Диагональ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(32, 102);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Периметр";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(185, 58);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(88, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Площадь_В";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(32, 58);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Площадь_О";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ WinFormsApp5/WinFormsApp5/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            t = point;
+            label1.Text = "Активный объект: точка";
+        }
+        Point point = new Point();
+        RegularPentagon pent = new RegularPentagon();
+        RightTriangle triang = new RightTriangle();
+        Rectangle rect = new Rectangle();
+        Point t;
+
+        public class Point
+        {
+            public float Square()
+            {
+                return 0;
+            }
+
+            public virtual float SquareVirt()
+            {
+                return 0;
+            }
+        }
+
+        public class RegularPentagon : Point
+        {
+            private float side;
+            public RegularPentagon()
+            {
+                side = 3;
+            }
+
+            public float Square()
+            {
+                float S = (float)(5 / 4 * side * side * (1 / Math.Tan(Math.PI / 5)));
+                return S;
+            }
+
+            public override float SquareVirt()
+            {
+                float S = (float)(5 / 4 * side * side * (1 / Math.Tan(Math.PI / 5)));
+                return S;
+            }
+
+            public float Perimeter()
+            {
+                float P = side * 5;
+                return P;
+            }
+        }
+
+        public class RightTriangle : Point
+        {
+            private float firstCathet;
+            private float secondCathet;
+            public RightTriangle()
+            {
+                firstCathet = 3;
+                secondCathet = 4;
+            }
+            public float Square()
+            {
+                float S = 0.5f * firstCathet * secondCathet;
+                return S;
+            }
+
+            public override float SquareVirt()
+            {
+                float S = 0.5f * firstCathet * secondCathet;
+                return S;
+            }
+
+            public float Perimeter()
+            {
+                float P = (float)(firstCathet + secondCathet + Math.Sqrt(Math.Pow(firstCathet, 2) + Math.Pow(secondCathet, 2)));
+                return P;
+            }
+        }
+
+        public class Rectangle : Point
+        {
+            private float firstSide;
+            private float secondSide;
+            public Rectangle()
+            {
+                firstSide = 5;
+                secondSide = 6;
+            }
+            public float Square()
+            {
+                float S = firstSide * secondSide;
+                return S;
+            }
+
+            public override float SquareVirt()
+            {
+                float S = firstSide * secondSide;
+                return S;
+            }
+            public float Perimeter()
+            {
+                float P = firstSide * 2 + secondSide * 2;
+                return P;
+            }
+            public float Diag()
+            {
+                float D = (float)(Math.Sqrt(Math.Pow(firstSide, 2) + Math.Pow(secondSide, 2)));
+                return D;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = point;
+            label1.Text = "Активный объект: точка";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t = pent;
+            label1.Text = "Активный объект: правильный пятиугольник";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            t = triang;
+            label1.Text = "Активный объект: прямоугольный треугольник";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            t = rect;
+            label1.Text = "Активный объект: прямоугольник";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (t is RegularPentagon)
+            {
+                label2.Text = ((RegularPentagon)t).Square().ToString();
+            }
+            else if (t is RightTriangle)
+            {
+                label2.Text = ((RightTriangle)t).Square().ToString();
+            }
+            else if (t is Rectangle)
+            {
+                label2.Text = ((Rectangle)t).Square().ToString();
+            }
+            else if (t is Point)
+            {
+                label2.Text = t.Square().ToString();
+            }
+            else
+                label2.Text = "Метод Square() для активного объекта не определен";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label2.Text = t.SquareVirt().ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (t is RegularPentagon)
+            {
+                label2.Text = ((RegularPentagon)t).Perimeter().ToString();
+            }
+            else if (t is RightTriangle)
+            {
+                label2.Text = ((RightTriangle)t).Perimeter().ToString();
+            }
+            else if (t is Rectangle)
+            {
+                label2.Text = ((Rectangle)t).Perimeter().ToString();
+            }
+            else
+                label2.Text = "Метод Perimeter() для активного объекта не определен";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (t is Rectangle)
+            {
+                label2.Text = ((Rectangle)t).Diag().ToString();
+            }
+            else
+                label2.Text = "Метод Diag() для активного объекта не определен";
+        }
+    }
+}
